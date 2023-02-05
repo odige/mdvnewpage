d88302cad1baf94a94df407a9df76cb21382a7aa
odige <1905448814@qq.com>
2023-02-05 16:02:13 +0800

    style：格式化代码

--- src/main.js
+++ src/main.js
@@ -1,22 +1,18 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/js/bootstrap.min.js';
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.min.js";
 import { createRouter, createWebHashHistory } from "vue-router";
 
 // 在此添加路由页面
 import hello from "@/components/HelloWorld.vue";
 
 // 在此添加路由
-const routes = [
-    // 
-  {path: "/",name: "home",component: hello,
-  },
-];
+const routes = [{ path: "/", component: hello }];
 
 const router = createRouter({
-    history: createWebHashHistory(),
-    routes
-})
+  history: createWebHashHistory(),
+  routes,
+});
 
 createApp(App).use(router).mount("#app");
