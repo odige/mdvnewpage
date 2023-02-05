ba6dbee30fafd12285eee7ad6a068345ef4f7fc8
odige <1905448814@qq.com>
2023-02-05 15:41:27 +0800

    style：改写头部样式

--- src/main.js
+++ src/main.js
@@ -4,9 +4,12 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import { createRouter, createWebHashHistory } from "vue-router";
 
+// 在此添加路由页面
 import hello from "@/components/HelloWorld.vue";
 
+// 在此添加路由
 const routes = [
+    // 
   {path: "/",name: "home",component: hello,
   },
 ];
