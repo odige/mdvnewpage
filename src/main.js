fd0dbe1020cb40fac8bbfa246d1b46ffe29d2163
odige <1905448814@qq.com>
2023-02-12 18:51:17 +0800

    feat:添加路由

--- src/main.js
+++ src/main.js
@@ -4,12 +4,25 @@ import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 import { createRouter, createWebHashHistory } from "vue-router";
 
-
 // 在此添加路由页面
 import home from "@/components/PropaGanda.vue";
+import personal from "@/components/PerSonal.vue";
+import order from "@/components/OrDer.vue";
+import inregral from "@/components/InteGral.vue";
+import gift from "@/components/GiFt.vue";
+import delivergoods from "@/components/DeliverGoods.vue";
+import coin from "@/components/CoIn.vue";
 
 // 在此添加路由
-const routes = [{ path: "/", component: home }];
+const routes = [
+  { path: "/", component: home },
+  { path: "/personal", component: personal },
+  { path: "/order", component: order },
+  { path: "/inregral", component: inregral },
+  { path: "/gift", component: gift },
+  { path: "/delivergoods", component: delivergoods },
+  { path: "/coin", component: coin },
+];
 
 const router = createRouter({
   history: createWebHashHistory(),
